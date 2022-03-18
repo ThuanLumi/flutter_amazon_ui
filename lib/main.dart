9a2cbaa3a09dff25d3d4b1e0088baf02668c25b2
Thuan Dinh Nam <thuandinhnam307@gmail.com>
2022-03-18 16:31:45 +0700

    Add flutter_template_amazon_ui folder

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_template_amazon_ui/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Amazon UI Redesign',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryIconTheme: Theme.of(context).primaryIconTheme.copyWith(
+              color: Colors.black,
+            ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
